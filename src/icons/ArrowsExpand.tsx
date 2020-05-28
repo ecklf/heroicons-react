90660fb9922ac2484dc377245b15ddc3079ba307
impulse <ecklf@icloud.com>
2020-05-28 09:26:40 +0200

    fix: fix icon stroke v2

--- src/icons/ArrowsExpand.tsx
+++ src/icons/ArrowsExpand.tsx
@@ -16,13 +16,7 @@ const ArrowsExpand = (
       ref={svgRef}
       {...props}
     >
-      <path
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M3 8V4m0 0h4M3 4l4 4m8 0V4m0 0h-4m4 0l-4 4m-8 4v4m0 0h4m-4 0l4-4m8 4l-4-4m4 4v-4m0 4h-4"
-      />
+      <path d="M3 8V4m0 0h4M3 4l4 4m8 0V4m0 0h-4m4 0l-4 4m-8 4v4m0 0h4m-4 0l4-4m8 4l-4-4m4 4v-4m0 4h-4" />
     </svg>
   );
 };
