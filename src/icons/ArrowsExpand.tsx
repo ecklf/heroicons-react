6ad264c6584f9b8e71a97e4f471e9f6ecdca4bcb
impulse <ecklf@icloud.com>
2020-05-28 09:34:21 +0200

    fix: revert fixes and wait for official fix

--- src/icons/ArrowsExpand.tsx
+++ src/icons/ArrowsExpand.tsx
@@ -16,7 +16,13 @@ const ArrowsExpand = (
       ref={svgRef}
       {...props}
     >
-      <path d="M3 8V4m0 0h4M3 4l4 4m8 0V4m0 0h-4m4 0l-4 4m-8 4v4m0 0h4m-4 0l4-4m8 4l-4-4m4 4v-4m0 4h-4" />
+      <path
+        stroke="#374151"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M3 8V4m0 0h4M3 4l4 4m8 0V4m0 0h-4m4 0l-4 4m-8 4v4m0 0h4m-4 0l4-4m8 4l-4-4m4 4v-4m0 4h-4"
+      />
     </svg>
   );
 };
--- src/icons/ShoppingBag.tsx
+++ src/icons/ShoppingBag.tsx
@@ -16,8 +16,17 @@ const ShoppingBag = (
       ref={svgRef}
       {...props}
     >
-      <path d="M5 8h10l1 9H4l1-9z" />
-      <path d="M7 6a3 3 0 013-3 3 3 0 013 3v3a3 3 0 01-3 3 3 3 0 01-3-3V6z" />
+      <path
+        stroke="#374151"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M5 8h10l1 9H4l1-9z"
+      />
+      <path
+        stroke="#374151"
+        strokeWidth={2}
+        d="M7 6a3 3 0 013-3v0a3 3 0 013 3v3a3 3 0 01-3 3v0a3 3 0 01-3-3V6z"
+      />
       <rect width={2} height={2} x={6} y={9} rx={1} />
       <rect width={2} height={2} x={12} y={9} rx={1} />
     </svg>
